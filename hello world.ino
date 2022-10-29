c72740d60064fc52bed2b635015aedb8adabf9e8
psume <113950613+psume@users.noreply.github.com>
2022-10-28 19:57:19 -0500

    Add files via upload

---
+++ hello world.ino
@@ -0,0 +1,12 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(2000000);
+  while(!Serial);
+
+  Serial.println("Howdy World!");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
